b2b8e97bbb15d3ce38887cf723a9eb6ea17c49fc
mmhot <mm.moazzen@hotmail.com>
2025-05-20 18:51:45 +0330

    Add files via upload

---
+++ src/user_settings.py
@@ -0,0 +1,32 @@
+# Please modify the settings below according to your needs.
+
+# List of source URLs to fetch proxy configurations from.
+# Add or remove URLs as needed. All URLs in this list are automatically enabled.
+SOURCE_URLS = [
+    "https://getafreenode.com/subscribe/?uuid=ebbeef45-0fd8-40cd-aca5-1e2ce18abeff",
+    # Add more URLs here if you want to include additional sources.
+]
+
+# Set to True to fetch the maximum possible number of configurations.
+# If True, SPECIFIC_CONFIG_COUNT will be ignored.
+USE_MAXIMUM_POWER = True
+
+# Desired number of configurations to fetch.
+# This is used only if USE_MAXIMUM_POWER is False.
+SPECIFIC_CONFIG_COUNT = 1000
+
+# Dictionary of protocols to enable or disable.
+# Set each protocol to True to enable, False to disable.
+ENABLED_PROTOCOLS = {
+    "wireguard://": False,
+    "hysteria2://": True,
+    "vless://": False,
+    "vmess://": False,
+    "ss://": False,
+    "trojan://": False,
+    "tuic://": False,
+}
+
+# Maximum age of configurations in days.
+# Configurations older than this will be considered invalid.
+MAX_CONFIG_AGE_DAYS = 365
